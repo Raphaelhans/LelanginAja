3daa15ce76d5d5b09510ba88009f4830669e77ff
JoshuaNathan1 <joshuanatham16@gmail.com>
2025-05-26 16:50:58 +0700

    Perubahan Table bids -> transaksi

--- Project/app/src/main/java/com/example/project/ui/auction/Bid.kt
+++
@@ -1,9 +0,0 @@
-package com.example.project.ui.auction
-
-data class Bid(
-    val bidId: String = "",
-    val auctionItemId: Int = 0,
-    val userEmail: String = "",
-    val amount: Double = 0.0,
-    val timestamp: Long = System.currentTimeMillis()
-)
